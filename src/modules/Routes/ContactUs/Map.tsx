bc47eb0b2c056b2e5ee3bda6cb08a4de91806532
damian-pebe <damian.pebe@gmail.com>
2025-01-27 17:18:56 -0600

    added map, works?

--- src/modules/Routes/ContactUs/Map.tsx
+++ src/modules/Routes/ContactUs/Map.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
 const MapComponent = () => {
-  
-    return (
-  
-    <div>
-        <h1>Map here</h1>
-    </div>
-    );
-  };
-  
-  export default MapComponent;
+  useEffect(() => {
+    // Initialize the map
+    const map = L.map("map").setView([51.505, -0.09], 13); // Centered at London, zoom level 13
+
+    // Add a tile layer (OpenStreetMap by default)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Add a marker
+    L.marker([51.505, -0.09]).addTo(map).bindPopup("A sample marker").openPopup();
+
+    return () => {
+      map.remove(); // Clean up map on component unmount
+    };
+  }, []);
+
+  return <div id="map" style={{ height: "500px", width: "100%" }}></div>;
+};
+
+export default MapComponent;
