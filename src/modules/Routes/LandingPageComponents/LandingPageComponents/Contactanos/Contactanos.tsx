7657f1ba76ffd2186eb427210da7d5406986a9be
damian-pebe <damian.pebe@gmail.com>
2025-02-09 23:02:11 -0600

    (Mod): Changed cards of contact a single ceo from company.

--- src/modules/Routes/LandingPageComponents/LandingPageComponents/Contactanos/Contactanos.tsx
+++ src/modules/Routes/LandingPageComponents/LandingPageComponents/Contactanos/Contactanos.tsx
@@ -4,16 +4,22 @@ import "./Contactanos.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import FormContact from "./components/FormContact";
 import { PinContainer } from "@/components/ui/3d-pin";
-import facebook from "@/assets/facebook.png";
-import whatsapp from "@/assets/whatsapp.webp";
-import instagram from "@/assets/instagram.webp";
-import email from "@/assets/email.png";
+
 import ceo1 from "@/assets/rb_174457.png";
 import ceo2 from "@/assets/rb_176267.png";
-import { Mail, Phone } from "lucide-react";
 import { Popover } from "antd";
-import styled from "styled-components";
 import Socials from "./components/Socials";
+import {
+  Whatsapp,
+  EnvelopeFill,
+  TelephoneFill,
+  Facebook,
+  Instagram,
+  Globe,
+} from "react-bootstrap-icons";
+import TrueFocus from "@/components/reactbits/true_focus";
+import SpotlightCard from "@/components/reactbits/spotlight_card";
+import { Button } from "@/components/ui/button";
 
 export default function Contactanos() {
   return (
@@ -25,32 +31,33 @@ export default function Contactanos() {
           <FormContact />
         </div>
 
-        
         <div className=" md:block hidden flex-1  transition-all duration-1000">
-          <h3 className="py-16 justify-center flex items-center scroll-m-20 text-4xl font-extralight font-zilla">
-            Nuestras redes sociales
-          </h3>
-          <AnimatedPinDemo />
-          <div className="py-16 flex justify-center w-full ">
-            <Socials />
+          <div className="pb-8 justify-center flex items-center scroll-m-20 text-4xl font-extralight font-raleway">
+            <TrueFocus
+              sentence="Redes sociales"
+              manualMode={false}
+              blurAmount={5}
+              borderColor="rgba(29, 204, 206, 0.8)"
+              animationDuration={2}
+              pauseBetweenAnimations={0.5}
+            />
           </div>
+          <AnimatedPinDemo />
         </div>
         <div className="sm:block md:hidden py-8 flex justify-center w-full ">
-            <Socials />
-          </div>
+          <Socials />
+        </div>
       </div>
       <div className="md:hidden block">
         <DividerNoSpacing />
       </div>
       <ContactData />
       <Divider />
-      
     </div>
   );
 }
 
 function TitleSolidGradiant() {
-  
   return (
     <div className="flex items-center justify-center w-full flex-col px-4 ">
       <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-t  from-neutral-600 to-white text-7xl lg:text-8xl py-2 relative z-20 font-bold font-poppins tracking-tight">
@@ -58,7 +65,7 @@ function TitleSolidGradiant() {
         LUXOFLEX.
       </h2>
       <p className=" mx-auto text-lg md:text-xl text-neutral-700 dark:text-neutral-400 text-center pb-5">
-      ¡Cuéntanos tu idea, nosotros la haremos realidad!
+        ¡Cuéntanos tu idea, nosotros la haremos realidad!
       </p>
     </div>
   );
@@ -68,7 +75,7 @@ function AnimatedPinDemo() {
   return (
     <div>
       <div className="">
-        <div className="grid md:grid-cols-5 grid-cols-2 gap-10 justify-center">
+        <div className="grid grid-cols-3 gap-10 justify-center">
           <PinContainer title="/luxoflex.vercel.app" href="luxoflex.vercel.app">
             <div className="flex basis-full flex-col p-2 tracking-tighter text-slate-100/50 sm:basis-1/2 w-[10rem] h-[15rem]">
               <h3 className="max-w-xs !pb-1 !m-0 font-extralight font-zilla text-base text-slate-100">
@@ -79,11 +86,24 @@ function AnimatedPinDemo() {
                   LUXOFLEX tiene contacto por medio de Email
                 </span>
               </div>
-              <div className="flex flex-1 w-full rounded-lg mt-2 bg-gradient-to-br from-white via-gray-300 to-white justify-center items-center">
-                <img
-                  className="rounded-t-lg p-2 w-12 h-12 object-contain"
-                  src={email}
-                />
+              <div className="flex flex-1 w-full bg-gray-800 hover:bg-purple-900 duration-1000 rounded-lg mt-2  justify-center items-center">
+                <Globe className="h-12 w-12 text-purple-600" />
+              </div>
+            </div>
+          </PinContainer>
+
+          <PinContainer title="/luxoflex.vercel.app" href="luxoflex.vercel.app">
+            <div className="flex basis-full flex-col p-2 tracking-tighter text-slate-100/50 sm:basis-1/2 w-[10rem] h-[15rem]">
+              <h3 className="max-w-xs !pb-1 !m-0 font-extralight font-zilla text-base text-slate-100">
+                Email
+              </h3>
+              <div className="text-xs !m-0 !p-0 font-normal">
+                <span className="text-slate-500 text-sm">
+                  LUXOFLEX tiene contacto por medio de Email
+                </span>
+              </div>
+              <div className="flex flex-1 w-full bg-gray-800 hover:bg-red-900 duration-1000 rounded-lg mt-2  justify-center items-center">
+                <EnvelopeFill className="h-12 w-12 text-red-600" />
               </div>
             </div>
           </PinContainer>
@@ -98,8 +118,8 @@ function AnimatedPinDemo() {
                   LUXOFLEX tiene contacto por medio de telefono celular
                 </span>
               </div>
-              <div className="flex flex-1 w-full rounded-lg mt-2 bg-gradient-to-br from-white via-gray-300 to-white justify-center items-center">
-                <Phone className="w-12 h-12 text-black" />
+              <div className="flex flex-1 w-full bg-gray-800 hover:bg-teal-900 duration-1000 rounded-lg mt-2  justify-center items-center">
+                <TelephoneFill className="h-12 w-12 text-teal-600" />
               </div>
             </div>
           </PinContainer>
@@ -114,11 +134,8 @@ function AnimatedPinDemo() {
                   LUXOFLEX tiene contacto por medio de WhatsApp
                 </span>
               </div>
-              <div className="flex flex-1 w-full rounded-lg mt-2 bg-gradient-to-br from-white via-gray-300 to-white justify-center items-center">
-                <img
-                  className="rounded-t-lg w-12 h-12 object-contain"
-                  src={whatsapp}
-                />
+              <div className="flex flex-1 w-full bg-gray-800 hover:bg-green-900 duration-1000 rounded-lg mt-2  justify-center items-center">
+                <Whatsapp className="h-12 w-12 text-green-600" />
               </div>
             </div>
           </PinContainer>
@@ -133,11 +150,8 @@ function AnimatedPinDemo() {
                   LUXOFLEX tiene contacto por medio de Facebook
                 </span>
               </div>
-              <div className="flex flex-1 w-full rounded-lg mt-2 bg-gradient-to-br from-white via-gray-300 to-white justify-center items-center">
-                <img
-                  className="rounded-t-lg p-2 w-12 h-12 object-contain"
-                  src={facebook}
-                />
+              <div className="flex flex-1 w-full bg-gray-800 hover:bg-blue-900 duration-1000 rounded-lg mt-2  justify-center items-center">
+                <Facebook className="h-12 w-12 text-blue-600" />
               </div>
             </div>
           </PinContainer>
@@ -152,11 +166,8 @@ function AnimatedPinDemo() {
                   LUXOFLEX tiene contacto por medio de Instagram
                 </span>
               </div>
-              <div className="flex flex-1 w-full rounded-lg mt-2 bg-gradient-to-br from-white via-gray-300 to-white justify-center items-center">
-                <img
-                  className="rounded-t-lg p-2 w-12 h-12 object-contain"
-                  src={instagram}
-                />
+              <div className="flex flex-1 w-full rounded-lg mt-2 bg-gray-800 hover:bg-pink-900 duration-1000 justify-center items-center">
+                <Instagram className="h-12 w-12 text-pink-600" />
               </div>
             </div>
           </PinContainer>
@@ -176,7 +187,8 @@ function ContactData() {
       hrefFacebook: "https://facebook.com/martin",
       hrefInstagram: "https://instagram.com/martin",
       img: ceo1,
-      description: "CEO y Fundador de LUXOFLEX\nCon más de 15 años de experiencia en la industria de etiquetas autoadheribles",
+      description:
+        "CEO y Fundador de LUXOFLEX\nCon más de 15 años de experiencia en la industria de etiquetas autoadheribles",
     },
     {
       name: "Mayra Becerril",
@@ -189,7 +201,6 @@ function ContactData() {
       description:
         "CEO y Fundadora de LUXOFLEX\nCon más de 8 años de experiencia en etiquetas autoadheribles.",
     },
-   
   ];
   return (
     <div>
@@ -198,256 +209,103 @@ function ContactData() {
       </a>
       <div className=" flex flex-wrap justify-center items-center gap-6 ">
         {data.map((user) => (
-          <Card {...user} />
+          <SpotlightCard
+            className="custom-spotlight-card w-96 h-72"
+            spotlightColor="rgba(255, 255, 255, 0.2)"
+          >
+            <div className="flex flex-col justify-between gap-5">
+             
+                <span className="text-2xl font-audiowide">{user.name}</span>
+                <div className=" text-1xl font-audiowide text-base overflow-hidden">
+                  {user.description}
+                </div>
+              
+              <div className="bottom-bottom">
+                <div className="social-links-container">
+                  <Popover content="Llamar">
+                    <i
+                      onClick={() =>
+                        (window.location.href = `tel:+52${user.hrefPhone}`)
+                      }
+                      className="hover:cursor-pointer bi bi-telephone-outbound text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
+                    ></i>
+                  </Popover>
+
+                  <Popover content="WhatsApp">
+                    <i
+                      onClick={() =>
+                        (window.location.href = `https://wa.me/+52${user.hrefWhatsapp}`)
+                      }
+                      className="hover:cursor-pointer bi bi-whatsapp text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
+                    ></i>
+                  </Popover>
+
+                  <Popover content="Facebook">
+                    <i
+                      onClick={() => (window.location.href = user.hrefFacebook)}
+                      className="hover:cursor-pointer bi bi-facebook text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
+                    ></i>
+                  </Popover>
+
+                  <Popover content="Instagram">
+                    <i
+                      onClick={() =>
+                        (window.location.href = user.hrefInstagram)
+                      }
+                      className="hover:cursor-pointer bi bi-instagram text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
+                    ></i>
+                  </Popover>
+                </div>
+                <Button
+                  variant="default"
+                  className="px-10 py-2 hover:bg-black hover:text-white font-poppins font-light  transition-colors duration-1000"
+                  onClick={() =>
+                    (window.location.href = `tel:+52${user.hrefPhone}`)
+                  }
+                >
+                  Contactame
+                </Button>
+              </div>
+            </div>
+          </SpotlightCard>
         ))}
       </div>
     </div>
   );
 }
 
-const Card = ({
-  name,
-  hrefEmail,
-  hrefPhone,
-  hrefWhatsapp,
-  hrefFacebook,
-  hrefInstagram,
-  img,
-  description,
-}: {
-  name: string;
-  hrefEmail: string;
-  hrefPhone: string;
-  hrefWhatsapp: string;
-  hrefFacebook: string;
-  hrefInstagram: string;
-  img: string;
-  description: string;
-}) => {
-  return (
-    <StyledWrapper>
-      <div className="card">
-        <div className="mail">
-          <Popover content="Enviar email">
-            <Mail
-              onClick={() => (window.location.href = hrefEmail)}
-              className="hover:cursor-pointer transition-colors duration-1000 text-pink-300 hover:text-pink-800"
-            />
-          </Popover>
-        </div>
-        <div className="profile-pic">
-          <img
-            className=" w-full h-full object-top transition-all duration-500 hover:scale-150 hover:object-top"
-            src={img}
-          />
-        </div>
-        <div className="bottom">
-          <div className="content">
-            <span className="name font-audiowide">{name}</span>
-            <div className=" font-playfair text-base overflow-hidden">{description}</div>
-          </div>
-          <div className="bottom-bottom">
-            <div className="social-links-container">
-              <Popover content="Llamar">
-                <i
-                  onClick={() => (window.location.href = `tel:+52${hrefPhone}`)}
-                  className="hover:cursor-pointer bi bi-telephone-outbound text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
-                ></i>
-              </Popover>
+// const Card = ({
+//   name,
+//   hrefEmail,
+//   hrefPhone,
+//   hrefWhatsapp,
+//   hrefFacebook,
+//   hrefInstagram,
+//   img,
+//   description,
+// }: {
+//   name: string;
+//   hrefEmail: string;
+//   hrefPhone: string;
+//   hrefWhatsapp: string;
+//   hrefFacebook: string;
+//   hrefInstagram: string;
+//   img: string;
+//   description: string;
+// }) => {
+//   return (
+//     <div>
+//       <div className="card">
+//         <div className="mail">
+//           <Popover content="Enviar email">
+//             <Mail
+//               onClick={() => (window.location.href = hrefEmail)}
+//               className="hover:cursor-pointer transition-colors duration-1000 text-pink-300 hover:text-pink-800"
+//             />
+//           </Popover>
+//         </div>
 
-              <Popover content="WhatsApp">
-                <i
-                  onClick={() =>
-                    (window.location.href = `https://wa.me/+52${hrefWhatsapp}`)
-                  }
-                  className="hover:cursor-pointer bi bi-whatsapp text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
-                ></i>
-              </Popover>
-
-              <Popover content="Facebook">
-                <i
-                  onClick={() => (window.location.href = hrefFacebook)}
-                  className="hover:cursor-pointer bi bi-facebook text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
-                ></i>
-              </Popover>
-
-              <Popover content="Instagram">
-                <i
-                  onClick={() => (window.location.href = hrefInstagram)}
-                  className="hover:cursor-pointer bi bi-instagram text-lg  transition-all duration-1000 text-white hover:text-pink-800 hover:scale-150"
-                ></i>
-              </Popover>
-            </div>
-            <button
-              className="button font-playfair font-bold transition-colors duration-1000"
-              onClick={() => (window.location.href = `tel:+52${hrefPhone}`)}
-            >
-              Contactame
-            </button>
-          </div>
-        </div>
-      </div>
-    </StyledWrapper>
-  );
-};
-
-const StyledWrapper = styled.div`
-  .card {
-    width: 280px;
-    height: 280px;
-    background: white;
-    border-radius: 32px;
-    padding: 3px;
-    position: relative;
-    box-shadow: #604b4a30 0px 70px 30px -50px;
-    transition: all 0.5s ease-in-out;
-    overflow: hidden;
-  }
-
-  .card .mail {
-    position: absolute;
-    right: 2rem;
-    top: 1.4rem;
-    background: transparent;
-    border: none;
-  }
-
-  .card .mail img {
-    stroke: #fbb9b6;
-    stroke-width: 3px;
-  }
-
-  .card .mail img:hover {
-    stroke: #f55d56;
-  }
-
-  .card .profile-pic {
-    position: absolute;
-    width: calc(100% - 6px);
-    height: calc(100% - 6px);
-    top: 3px;
-    left: 3px;
-    border-radius: 29px;
-    z-index: 1;
-    border: 0px solid #fbb9b6;
-    overflow: hidden;
-    transition: all 0.5s ease-in-out;
-  }
-
-  .card .profile-pic img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    object-position: center;
-    transition: all 0.5s ease-in-out;
-  }
-
-  .card:hover .profile-pic {
-    width: 100px;
-    height: 100px;
-    top: 10px;
-    left: 10px;
-    border-radius: 50%;
-    z-index: 3;
-    border: 7px solid #fbb9b6;
-    box-shadow: rgba(96, 75, 74, 0.1882352941) 0px 5px 5px 0px;
-  }
-
-  .card:hover .profile-pic img {
-    object-fit: cover;
-    object-position: top;
-    height: 300%;
-    width: 100%;
-  }
-
-  .card .bottom {
-    position: absolute;
-    bottom: 3px;
-    left: 3px;
-    right: 3px;
-    background: #fbb9b6;
-    top: 80%;
-    border-radius: 29px;
-    z-index: 2;
-    box-shadow: rgba(96, 75, 74, 0.1882352941) 0px 5px 5px 0px inset;
-    overflow: hidden;
-    transition: all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) 0s;
-  }
-
-  .card .bottom .content {
-    position: absolute;
-    bottom: 0;
-    left: 1.5rem;
-    right: 1.5rem;
-    height: 160px;
-  }
-
-  .card .bottom .content .name {
-    display: block;
-    font-size: 1.2rem;
-    color: white;
-    font-weight: bold;
-  }
-
-  .card .bottom .content .about-me {
-    display: block;
-    font-size: 0.9rem;
-    color: white;
-    margin-top: 1rem;
-  }
-
-  .card .bottom .bottom-bottom {
-    position: absolute;
-    bottom: 1rem;
-    left: 1.5rem;
-    right: 1.5rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .card .bottom .bottom-bottom .social-links-container {
-    display: flex;
-    gap: 1rem;
-  }
-
-  .card .bottom .bottom-bottom .social-links-container img {
-    height: 20px;
-    fill: white;
-    filter: drop-shadow(0 5px 5px rgba(165, 132, 130, 0.1333333333));
-  }
-
-  .card .bottom .bottom-bottom .social-links-container img:hover {
-    fill: #f55d56;
-    transform: scale(1.2);
-  }
-
-  .card .bottom .bottom-bottom .button {
-    background: white;
-    color: #fbb9b6;
-    border: none;
-    border-radius: 20px;
-    font-size: 0.6rem;
-    padding: 0.4rem 0.6rem;
-    box-shadow: rgba(165, 132, 130, 0.1333333333) 0px 5px 5px 0px;
-  }
-
-  .card .bottom .bottom-bottom .button:hover {
-    background: #f55d56;
-    color: white;
-  }
-
-  .card:hover {
-    border-top-left-radius: 55px;
-  }
-
-  .card:hover .bottom {
-    top: 20%;
-    border-radius: 80px 29px 29px 29px;
-    transition: all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) 0.2s;
-  }
-
-  .profile-pic {
-    overflow: hidden;
-  }
-`;
+//       </div>
+//     </div>
+//   );
+// };
